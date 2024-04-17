1285e398cf80d84b8f175a12bc965b7ac733e7e9
Alistair McCann <allyjmccann@gmail.com>
2024-04-17 11:14:09 +0200

    en kommit

--- myMovieDatabase_Grupp/src/App.jsx
+++ myMovieDatabase_Grupp/src/App.jsx
@@ -1,11 +1,15 @@
+import './App.css'
+import Homepage from './pages/homepage/Homepage.jsx'
 
 
 function App() {
 
   return (
-    <>
-      
-    </>
+
+    <div className="app">
+      <Homepage />
+    </div>
+
   )
 }
 
