3eb57d5ff26cb289711119fc5c935fe9a02daa77
ronnedahl <smartareshopping@gmail.com>
2024-04-17 13:21:05 +0200

    Update App.jsx

--- myMovieDatabase_Grupp/src/App.jsx
+++ myMovieDatabase_Grupp/src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css'
 import Homepage from './pages/homepage/Homepage.jsx'
-
+import NavigationHeader from "./navigation/NavigationHeader"
 
 function App() {
 
   return (
 
     <div className="app">
+       <NavigationHeader/>
       <Homepage />
     </div>
 
   )
+
 }
 
 export default App
