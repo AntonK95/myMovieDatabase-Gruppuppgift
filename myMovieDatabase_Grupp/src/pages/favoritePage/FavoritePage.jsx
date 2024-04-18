e3e1b09c006ff2fc9b5ada2eaa0306c26c6dc992
Anton Knutsson <cykelkorg95@gmail.com>
2024-04-18 11:26:39 +0200

    structure for search result page

--- myMovieDatabase_Grupp/src/pages/favoritePage/FavoritePage.jsx
+++ myMovieDatabase_Grupp/src/pages/favoritePage/FavoritePage.jsx
@@ -6,28 +6,28 @@ function FavoritePage() {
   return (
     <section className='favorite__container'>
         <div className="favorite__movie-card">
-            
+            <p>Favorite Movie</p>
         </div>
         <div className="favorite__movie-card">
-            
+            <p>Favorite Movie</p>
         </div>
         <div className="favorite__movie-card">
-            
+            <p>Favorite Movie</p>
         </div>
         <div className="favorite__movie-card">
-            
+            <p>Favorite Movie</p>
         </div>
         <div className="favorite__movie-card">
-            
+           <p>Favorite Movie</p> 
         </div>
         <div className="favorite__movie-card">
-            
+           <p>Favorite Movie</p> 
         </div>
         <div className="favorite__movie-card">
-            
+            <p>Favorite Movie</p>
         </div>
         <div className="favorite__movie-card">
-            
+            <p>Favorite Movie</p>
         </div>
     </section>
   )
---
+++ myMovieDatabase_Grupp/src/pages/searchResultsPage/SearchResultsPage.jsx
@@ -0,0 +1,35 @@
+import './searchresultspage.css';
+
+
+function SearchResultsPage() {
+  return (
+    <section className='search-result__container'>
+        <div className="search-result__movie-card">
+            <p>Search Result</p>
+        </div>
+        <div className="search-result__movie-card">
+            <p>Search Result</p>
+        </div>
+        <div className="search-result__movie-card">
+            <p>Search Result</p>
+        </div>
+        <div className="search-result__movie-card">
+            <p>Search Result</p>
+        </div>
+        <div className="search-result__movie-card">
+            <p>Search Result</p>
+        </div>
+        <div className="search-result__movie-card">
+            <p>Search Result</p>
+        </div>
+        <div className="search-result__movie-card">
+            <p>Search Result</p> 
+        </div>
+        <div className="search-result__movie-card">
+            <p>Search Result</p>
+        </div>
+    </section>
+  )
+}
+
+export default SearchResultsPage;
