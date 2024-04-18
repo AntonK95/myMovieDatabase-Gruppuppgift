8bb5d2fe7103dcb2939150d9f2c968a4aa26960d
Anton Knutsson <cykelkorg95@gmail.com>
2024-04-18 09:40:28 +0200

    qiucksave before merge

--- myMovieDatabase_Grupp/src/App.jsx
+++ myMovieDatabase_Grupp/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Homepage from './pages/homepage/Homepage.jsx'
 import Footer from './components/footer/Footer'
 import NavigationHeader from './components/header/NavigationHeader.jsx';
+import WatchlistPage from './pages/watchlistPage/WatchlistPage.jsx';
 
 function App() {
 
@@ -13,7 +14,7 @@ function App() {
     <div className="app">
       <NavigationHeader />
       <Homepage />
-      
+      <WatchlistPage />
       <Footer />
     </div>
 
---
+++ myMovieDatabase_Grupp/src/pages/watchlistPage/WatchlistPage.jsx
@@ -0,0 +1,35 @@
+import './watchlistpage.css'
+
+
+function WatchlistPage() {
+  return (
+    <section className='watchlist__container'>
+        <div className="watchlist__movie-card">
+            
+        </div>
+        <div className="watchlist__movie-card">
+            
+        </div>
+        <div className="watchlist__movie-card">
+            
+        </div>
+        <div className="watchlist__movie-card">
+            
+        </div>
+        <div className="watchlist__movie-card">
+            
+        </div>
+        <div className="watchlist__movie-card">
+            
+        </div>
+        <div className="watchlist__movie-card">
+            
+        </div>
+        <div className="watchlist__movie-card">
+            
+        </div>
+    </section>
+  )
+}
+
+export default WatchlistPage
