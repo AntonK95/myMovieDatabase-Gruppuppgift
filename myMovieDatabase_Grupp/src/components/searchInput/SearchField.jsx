39a814859f83e5febc4c33260142c623bf6d88ce
Anton Knutsson <cykelkorg95@gmail.com>
2024-04-23 08:49:40 +0200

    can now render searchResults, only in the wrong place

--- myMovieDatabase_Grupp/src/components/searchInput/SearchField.jsx
+++ myMovieDatabase_Grupp/src/components/searchInput/SearchField.jsx
@@ -1,11 +1,13 @@
 
 import { useState } from 'react';
 import axios from 'axios';
+import MovieCard from '../movieCard/MovieCard';
 
 const apiKey = '567f8027';
 
 function SearchField({ onSearch }) {
-    const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+    const [searchTerm, setSearchTerm] = useState([]);
 
     const handleSearchInput = (event) => {
       setSearchTerm(event.target.value);
@@ -17,15 +19,44 @@ function SearchField({ onSearch }) {
       event.preventDefault();
       try {
         const response = await axios.get(`https://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`);
-        console.log(response.data);
+        setSearchResults(response.data.Search);
+        console.log(response.data.Search);
       } catch(error) {
         console.log(error);
       }
+
     };
   return (
-    <form onSubmit={handleSearchSubmit}>
-        <input type="text" placeholder='Sök' value={searchTerm} onChange={handleSearchInput}></input>
-    </form>
+    <>
+      <div>
+        <form onSubmit={handleSearchSubmit}>
+            <input 
+              type="text" 
+              placeholder='Sök' 
+              value={searchTerm} 
+              onChange={handleSearchInput}>
+            </input>
+        </form>
+        </div>
+        <div>
+        {searchResults.map((result, index) => (
+          <MovieCard 
+            key={ index }
+            title={result.Title}
+            poster={result.Poster}
+          />
+        ))}
+        {/* <div>
+          {searchResults.map((results, index) => (
+            <div key={ index }>
+              <h5>{ results.Title }</h5>
+              <img src={ results.Poster } alt={ results.Title } />
+            </div>
+          ))}
+        </div> */}
+        </div>
+      
+    </>
   )
 }
 
